1b79c32deb178357808ecc9fbe3d7a7163fd0355
jonathan poelen <jonathan.poelen@gmail.com>
2016-02-04 05:27:55 +0100

    show backward transition in print_automaton

--- falcon/regex_dfa/print_automaton.cpp
+++ falcon/regex_dfa/print_automaton.cpp
@@ -129,91 +129,158 @@ void print_automaton(const Ranges& rngs)
   }
   
   struct L { std::size_t n; enum { None, Start, Plug, Stop, Cont } e; };
-  std::vector<std::vector<L>> graph;
-  auto add_column = [&graph, sz]{
-    graph.emplace_back();
-    graph.back().resize(sz, L{~std::size_t{}, L::None});
-  };
-  add_column();
-
-  unsigned i = 1;
-  auto first_rng = begin(rngs);
-  auto last_rng = end(rngs);
-  for (; first_rng != last_rng; ++first_rng) {
-    auto rng_num = std::size_t(first_rng - begin(rngs));
-    
-    auto first_trs = begin(first_rng->transitions);
-    auto last_trs = end(first_rng->transitions);
-    for (; first_trs != last_trs; ++first_trs, ++i) {
-      auto next = first_trs->next;
-      if (rng_num >= next) {
-        continue;
-      }
-      auto pgraph = std::find_if(begin(graph), end(graph), [i, next](auto & v){
-        return v[i].n == next;
-      });
-      if (end(graph) == pgraph) {
-        auto p = std::find_if(begin(graph), end(graph), [i](auto & v){
-          return v[i].e == L::None;
-        });
-        if (end(graph) == p) {
-          add_column();
-          p = end(graph)-1;
-        }
-        
-        std::size_t n = end(first_rng->transitions) - first_trs;
-        auto first_rng2 = first_rng;
-        while (unsigned(++first_rng2 - begin(rngs)) != next) {
-          n += first_rng2->transitions.size() + 2;
-        }
-        
-        auto first = &(*p)[i];
-        *first = L{next, L::Start};
-        *std::fill_n(++first, n, L{next, L::Cont}) = L{next, L::Stop};
-      }
-      else {
-        (*pgraph)[i].e = L::Plug;
-      }
-    }
-    i += 2;
-  }
+  using Graph = std::vector<std::vector<L>>;
   
-  auto igraph = 0;
-  auto print_tree =[&igraph, &graph]{
-    constexpr char const * borders[]{"  ", "┌─", "├─", "└─", "│ "};
-    unsigned i = 0;
-    unsigned pluged_color = nb_colors;
-    auto first = rbegin(graph);
-    auto last = rend(graph);
-    std::cout << " ";
-    for (; first != last; ++first) {
-      auto const e = (*first)[igraph].e;
-      if (e) {
-        std::cout << colors[i % nb_colors];        
+  auto process_graph = [sz, &rngs](Graph & graph, auto next_filter_op, auto f_ok, auto process_line){
+    auto add_column = [sz, &graph](){
+      graph.emplace_back();
+      graph.back().resize(sz, L{~std::size_t{}, L::None});
+    };
+    add_column();
+    
+    unsigned i = 1;
+    auto first_rng = begin(rngs);
+    auto last_rng = end(rngs);
+    for (; first_rng != last_rng; ++first_rng) {
+      auto rng_num = std::size_t(first_rng - begin(rngs));
+      
+      auto first_trs = begin(first_rng->transitions);
+      auto last_trs = end(first_rng->transitions);
+      for (; first_trs != last_trs; ++first_trs, ++i) {
+        auto next = first_trs->next;
+        if (next_filter_op(rng_num, next)) {
+          continue;
+        }
+        auto pgraph = std::find_if(begin(graph), end(graph), [i, next](auto & v){
+          return v[i].n == next;
+        });
+        if (end(graph) == pgraph) {
+          auto p = begin(graph);
+          do {
+            p = std::find_if(p, end(graph), [i](auto & v){
+              return v[i].e == L::None;
+            });
+          } while (p != end(graph) && !f_ok(&(*p)[i], first_rng, first_trs) && (++p, 1));
+          if (end(graph) == p) {
+            add_column();
+            p = end(graph)-1;
+          }
+          
+          process_line(&(*p)[i], first_rng, first_trs);
+        }
+        else {
+          (*pgraph)[i].e = L::Plug;
+        }
       }
-      std::cout << borders[e];
-      if (L::Start <= e && L::Stop >= e) {
-        pluged_color = i % nb_colors;
-        ++first;
-        break;
-      }
-      ++i;
+      i += 2;
     }
-    for (; first != last; ++first) {
-      ++i;
-      auto const e = (*first)[igraph].e;
-      if (e) {
-        std::cout << colors[i % nb_colors] << "│" << colors[pluged_color] << "─";
+  };
+  
+  Graph graph;
+  process_graph(
+    graph, std::greater_equal<>{}, 
+    [](auto, auto, auto){ return std::true_type{}; }, 
+    [&rngs](auto p, auto it_rng, auto it_trs){
+      auto next = it_trs->next;
+      std::size_t n = end(it_rng->transitions) - it_trs;
+      auto first_rng2 = it_rng;
+      auto last_rng2 = begin(rngs) + next;
+      while (++first_rng2 != last_rng2) {
+        n += first_rng2->transitions.size() + 2;
+      }
+      
+      *p = L{next, L::Start};
+      *std::fill_n(++p, n, L{next, L::Cont}) = L{next, L::Stop};
+    }
+  );
+  Graph graph2;
+  bool empty_graph2 = true;
+  process_graph(
+    graph2, std::less<>{}, 
+    [&rngs](auto p, auto it_rng, auto it_trs){
+      p -= it_trs - begin(it_rng->transitions) + 1;
+      auto first_rng2 = begin(rngs) + it_trs->next;
+      for (; first_rng2 != it_rng; ++first_rng2) {
+        if (p->e != L::None) {
+          return false;
+        }
+        p -= first_rng2->transitions.size() + 2;
+      }
+      return true;
+    }, 
+    [&rngs, &empty_graph2](auto p, auto it_rng, auto it_trs){
+      auto next = it_trs->next;
+      std::size_t n = it_trs - begin(it_rng->transitions) + 1;
+      auto first_rng2 = begin(rngs) + next;
+      for (; first_rng2 != it_rng; ++first_rng2) {
+        n += first_rng2->transitions.size() + 2;
+      }
+      
+      *p = L{next, L::Stop};
+      p -= n;
+      *p = L{next, L::Start};
+      std::fill_n(++p, n-1, L{next, L::Cont});
+      empty_graph2 = false;
+    }
+  );
+
+  auto igraph = 0;
+  auto print_tree =[&igraph, &graph, &graph2, empty_graph2]{
+    auto print_graph = [&igraph](Graph const & graph, auto ecmp) {
+      constexpr char const * borders[]{"  ", "┌─", "├─", "└─", "│ "};
+
+      unsigned i = 0;
+      unsigned pluged_color = nb_colors;
+      bool is_stop = false;
+      auto first = rbegin(graph);
+      auto last = rend(graph);
+      std::cout << " ";
+      
+      for (; first != last; ++first) {
+        auto const e = (*first)[igraph].e;
+        if (e) {
+          std::cout << colors[i % nb_colors];        
+        }
+        std::cout << borders[e];
+        if (L::Start <= e && L::Stop >= e) {
+          pluged_color = i % nb_colors;
+          is_stop = (ecmp == e);
+          ++first;
+          break;
+        }
+        ++i;
+      }
+      
+      constexpr char const * borders2[]{"", "│", "│", "┴", "│"};
+      
+      for (; first != last; ++first) {
+        ++i;
+        auto const e = (*first)[igraph].e;
+        if (e) {
+          std::cout << colors[i % nb_colors] << borders2[e] << colors[pluged_color] << "─";
+        }
+        else {
+          std::cout << colors[pluged_color] << "──";        
+        }
+      }
+      
+      if (pluged_color == nb_colors) {
+        std::cout << " " << reset_color;
       }
       else {
-        std::cout << colors[pluged_color] << "──";        
+        std::cout << (is_stop ? "▸" : "─");
       }
+    };
+    
+    if (!empty_graph2) {
+      print_graph(graph2, L::Start);
+      std::cout << reset_color << "┆";      
     }
-    if (pluged_color == nb_colors) {
-      std::cout << reset_color;
-    }
+    print_graph(graph, L::Stop);
+    
     ++igraph;
   };
+  
   for (auto & rng : rngs) {
     print_tree();
     print_head_rng(rng, int(&rng-&rngs[0]));
